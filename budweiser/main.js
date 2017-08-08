0eb0e1e61f069d3523d8cd588365b484fafba4f6
Alexey Miroshnychenko <muzz0neasdqwe@gmail.com>
2017-08-08 10:44:39 +0300

    asd

--- budweiser/main.js
+++ budweiser/main.js
@@ -7,7 +7,6 @@ $(document).ready(function() {
 	$('#yes').click(function(){
 		$('.first').hide();
 		$('.second').fadeIn();
-		changerPrize();// запускает смену картинок 
 	});
 	$('#stop').click(function(){
 		$('.final').hide();
@@ -17,17 +16,26 @@ $(document).ready(function() {
 ///////////////////////////////рандомный приз////////////////////////////////
 	var prizeList = ["i7.png", "px.png", "card.png", "vch.png"];// массив для хранения имен картинок 
 
-	function changerPrize() {// поочередно выводит на экран картинки взятые с массива 
-		var listNum = 0
-		interval = setInterval(function() {
-		  var val = "url(img/"+prizeList[listNum]+")"; // добавляет к имени картинки путь
-		  $('.randomPrize').css('background-image', val); // выводит очередную картинку на экран 
-		  listNum++
-		  if(listNum >= prizeList.length){
-		  	listNum = 0;
-		  }
-		  //console.log(listNum);
-		}, 200);
+	function changerPrize() {// поочередно выводит на экран картинки взятые с массива
+
+		//////////////////подгрузка картинок в кэш//////////////////
+		for (i = 0; i < prizeList.length; i++){
+			var cache = "url(img/"+prizeList[i]+")";
+			$('.randomPrize').css('background-image', cache);
+		};
+		///////////////////////////////////////////////////
+		setTimeout(function(){
+			var listNum = 0
+			interval = setInterval(function() {
+			  var val = "url(img/"+prizeList[listNum]+")"; // добавляет к имени картинки путь
+			  $('.randomPrize').css('background-image', val); // выводит очередную картинку на экран 
+			  listNum++
+			  if(listNum >= prizeList.length){
+			  	listNum = 0;
+			  }
+			  //console.log(listNum);
+			}, 200);
+		}, 500);
 	};
 /////////////////////////////обработчик//////////////////////////////////////
 	document.querySelector('.gameField').onclick = function(event) {
@@ -39,17 +47,21 @@ $(document).ready(function() {
 	function open (node){// принимает event.target (где был клик)
 		if (!node.classList.contains("open") && counter < 2){
 			node.classList.add("open");
+			document.querySelector('.testYorLuck').innerHTML = 'Try again';
 			counter++;
 		}
 
 		else if(!node.classList.contains("open") && counter == 2){
 			node.classList.add("prize");
+			document.querySelector('.testYorLuck').innerHTML = 'Prize';
 			counter++;
 			setTimeout(function(){
 				$('.second').hide();
 				$('.final').fadeIn();
-			}, 500);
+				changerPrize(); // запускает смену картинок 
+			}, 700);
 		}
 	};
 	///////////////////////////////////////////////////////////////////////
+		
 });
