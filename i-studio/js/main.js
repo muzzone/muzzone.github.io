80ae5ba3ed4c2b09b3bd922ab10183b996f33567
Alexey Miroshnychenko <muzz0neasdqwe@gmail.com>
2017-10-01 12:19:59 +0300

    i-studio fixed

--- i-studio/js/main.js
+++ i-studio/js/main.js
@@ -11,4 +11,31 @@ $(document).ready(function() {
 	$('.cd-primary-nav').click(function() {
 		$("#menu").addClass("menuClose");
 	});
+
+	var win_h = $(window).height();
+	var win_w = $(window).width();
+
+	if (win_h < 899 || win_w < 766){
+		$('#navFirstPage').attr('href', '#section0');
+		$('#navAbout').attr('href', '#section1');
+		$('#navServices').attr('href', '#section2');
+		$('#navClients').attr('href', '#section4');
+		$('#navPrices').attr('href', '#section5');
+		$('#navContacts').attr('href', '#section6');
+
+		$('a[href*=#]').bind("click", function(e){
+	 	var anchor = $(this);
+	 	$('html, body').stop().animate({
+	 	scrollTop: $(anchor.attr('href')).offset().top
+	 	}, 300);
+	 	e.preventDefault();
+	 	});
+	};
+
+
+	window.onresize = function() {
+		if (($(window).height() < 899 && win_h > 899) || ($(window).width() < 766 && win_w > 766)){
+			location.reload();
+		};
+	};
 });
